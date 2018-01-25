19227294d4e0a1d6f535d851442c55f95555c7cc
Busyreadingsomething <oro1919@gmail.com>
2018-01-24 16:59:45 -0800

    Working brute force single N queens problem

--- src/solvers.js
+++ src/solvers.js
@@ -29,7 +29,7 @@ window.findNRooksSolution = function(n) {
   for (let row = 0; row < boardArray.length; row++) {
     
     // for loop looping through cols
-    for (let col = row; col < boardArray.length; col++) {
+    for (let col = 0; col < boardArray.length; col++) {
       // check to see if there's any conflict (hasRookConflict?)
       // console.log('toggling', row, col, board.rows());
       
@@ -64,7 +64,68 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
-
+  // debugger;
+  // declare initial row var
+  let initRow = 0;
+  // declare initial col var
+  let initCol = 0;
+  
+  // while initial row is less than n && solution is undefined
+  while (initRow < n && solution === undefined && n !== 0) {
+    
+    // create a new board
+    let board = new Board({n: n});
+    let boardArray = board.rows();
+    
+    // declare remainingRooks
+    let remainingQueens = n;
+    // toggle initial piece
+    board.togglePiece(initRow, initCol);
+    remainingQueens--;
+    
+    // for loop looping through rows
+    for (let row = 0; row < boardArray.length; row++) {
+      
+      // for loop looping through cols
+      for (let col = 0; col < boardArray.length; col++) {
+        // check to see if there's any conflict (hasRookConflict?)
+        // console.log('toggling', row, col, board.rows());
+        
+        // if row is initial row and col is initial col
+        if (row === initRow && col === initCol) {
+          // continue
+          continue;
+        }
+        
+        board.togglePiece(row, col);
+        if (!board.hasAnyQueensConflicts()) {
+          // if it doesn't, toggle at that position
+          // decrease remainingRooks by 1
+          remainingQueens -= 1;
+          break;
+          // what if remainingRooks never gets to 0??
+        } else {
+          board.togglePiece(row, col);
+        }
+      }
+    }
+    // check if remaining queens is 0
+    if (remainingQueens === 0) {
+      solution = board.rows();
+    }
+    
+    // if col is at edge
+    if (initCol === n - 1) {
+      // set col to 0
+      initCol = 0;
+      // increment row by one
+      initRow++;
+    } else {
+      // else increment col
+      initCol++;
+    }
+  }
+  solution = solution ? solution : new Board({n: n}).rows();
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
